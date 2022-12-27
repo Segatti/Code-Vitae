3ae45f0956c2dfa79a7d5847a526161536a0faca
Segatti <dev.vittor@gmail.com>
2022-12-27 20:00:20 -0300

    refactor: substituição do projeto dibs

--- data/projetos/dibs/js/menu-mobile.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $('.menu-pc').click(function() {
-    $('.menu-mobile').find('nav').slideToggle();
-  })
-
-  $('.menu-mobile nav a').click(function() {
-    $('.menu-mobile nav').fadeOut();
-  })
-})
