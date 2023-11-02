4a0e6259d834ecd167b7f36e3a6119936ff4500e
Vittor Feitosa <dev.vittor@gmail.com>
2023-11-02 15:33:10 -0400

    feat: init

---
+++ mobile/aluga_comigo/android/app/src/main/kotlin/com/example/aluga_comigo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aluga_comigo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile/aluga_comigo/lib/app/app_module.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(Injector i) {}
+
+  @override
+  void routes(RouteManager r) {
+  }
+}
